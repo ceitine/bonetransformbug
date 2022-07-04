c100cdb638b4cec4c64e8bff878b4beef27b70d8
ceitine <fluffy00gt@gmail.com>
2022-07-04 23:42:01 +0300

    files

---
+++ code/TestEntity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Sandbox;
+
+public partial class TestEntity : ModelEntity
+{
+	public bool SetPos { get; set; } = false;
+
+	public override void Spawn()
+	{
+		SetModel( "models/test.vmdl" );
+	}
+
+	[Event.Tick]
+	void tick()
+	{
+		SetBoneTransform( "Bone", new Transform( Vector3.Up * 50f * MathF.Abs( MathF.Sin( Time.Now * 10f ) ) ), false );
+
+		if ( SetPos )
+		{
+			var pos = Position;
+			Position = 0;
+			Position = pos;
+		}
+	}
+}
